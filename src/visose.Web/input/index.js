8c9920094c8c7cf2b2613701dc8d14d57ecac175
visose <vicente@visose.com>
2022-04-21 13:20:34 +0100

    Add analytics

--- src/visose.Web/input/index.js
+++
@@ -1,9 +0,0 @@
-﻿'use strict';
-
-(async () => {
-    const response = await fetch("/api/count", {
-        method: "post",
-        body: JSON.stringify({ page: "index" }),
-    });
-    console.log("Increment counter: ", response.statusText);
-})();
